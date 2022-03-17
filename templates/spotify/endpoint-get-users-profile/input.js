24002267c54e71fbeeadd03fdc2a2698c8819c6f
Paul Krishnamurthy <paul@paulkr.com>
2022-03-17 13:36:42 -0400

    Added Spotify templates

---
+++ templates/spotify/endpoint-get-users-profile/input.js
@@ -0,0 +1,27 @@
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Get a User's Profile [Input]
+ *
+ * @author    Buildable Technologies Inc.
+ * @access    open
+ * @license   MIT
+ * @docs      https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-users-profile
+ *
+ * ----------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * Lets you select the input for your Node's run function
+ *
+ * @param {Params} params
+ * @param {Object} $trigger - This Flow's request object
+ * @param {Object} $nodes - Data from above Nodes
+ */
+const nodeInput = ({ $trigger, $nodes }) => {
+  return {
+    SPOTIFY_CLIENT_ID: $trigger.env.SPOTIFY_CLIENT_ID, // Required
+    SPOTIFY_CLIENT_SECRET: $trigger.env.SPOTIFY_CLIENT_SECRET, // Required
+    SPOTIFY_BASE_URI: $trigger.env.SPOTIFY_BASE_URI, // Required
+    user_id: "string", // Required
+  };
+};
