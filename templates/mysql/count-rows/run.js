9ef69bfa037111f2f28a430dfbe17f17421c6cd3
Mike Gindin <gindin.mike@gmail.com>
2022-03-25 13:27:02 -0400

    Add MySQL node templates

---
+++ templates/mysql/count-rows/run.js
@@ -0,0 +1,55 @@
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Count Rows [Run]
+ *
+ * @description - Count number of rows from a MySQL table
+ *
+ * @author    Buildable Technologies Inc.
+ * @access    open
+ * @license   MIT
+ * @docs      https://knexjs.org
+ *
+ * ----------------------------------------------------------------------------------------------------
+ */
+
+const { getConnection } = require("@buildable/knex");
+
+/**
+ * The Node’s executable function
+ *
+ * @param {Run} input - Data passed to your Node from the input function
+ */
+const run = async (input) => {
+  const { MYSQL_CONNECTION_KEY, tableName, query = {} } = input;
+
+  verifyInput(input);
+
+  try {
+    const db = await getConnection(MYSQL_CONNECTION_KEY);
+
+    const total = Number.parseInt((await db(tableName).where(query).count({ count: "*" }))[0].count);
+
+    return total;
+  } catch (error) {
+    return {
+      failed: true,
+      message: error.message,
+      data: {},
+    };
+  }
+};
+
+const verifyInput = ({ MYSQL_CONNECTION_KEY, tableName, query }) => {
+  const ERRORS = {
+    NO_MYSQL_CONNECTION_KEY: `A valid MYSQL_CONNECTION_KEY is required. 
+                                    You can add one to your environment variables at 
+                                    https://app.buildable.dev/settings/environment-variables. 
+                                    You may also need to add a MySQL Datasource to your project.`,
+    NO_TABLE_NAME: "A valid tableName name is required.",
+    INVALID_QUERY: "The query must be an object.",
+  };
+
+  if (!MYSQL_CONNECTION_KEY) throw new Error(ERRORS.NO_MYSQL_CONNECTION_KEY);
+  if (!tableName || typeof tableName !== "string") throw new Error(ERRORS.NO_TABLE_NAME);
+  if (query && typeof query !== "object") throw new Error(ERRORS.INVALID_QUERY);
+};
