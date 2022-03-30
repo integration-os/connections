dee711dc893dc47b376160c01f499798e42abfe3
Mike Gindin <gindin.mike@gmail.com>
2022-03-30 16:19:10 -0400

    Add Firestore node templates

---
+++ templates/firestore/get-document/run.js
@@ -0,0 +1,68 @@
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Get Document [Run]
+ *
+ * @description - Get a document by its ID from a Firestore collection
+ *
+ * @author    Buildable Technologies Inc.
+ * @access    open
+ * @license   MIT
+ * @docs      https://firebase.google.com/docs/firestore/query-data/get-data#get_a_document
+ *
+ * ----------------------------------------------------------------------------------------------------
+ */
+
+const { getConnection } = require("@buildable/firestore");
+
+const DOCUMENT_NOT_FOUND_ERROR = "Document not found.";
+
+/**
+ * The Node’s executable function
+ *
+ * @param {Run} input - Data passed to your Node from the input function
+ */
+const run = async (input) => {
+  const { FIRESTORE_CONNECTION_KEY, collection, id } = input;
+
+  verifyInput(input);
+
+  try {
+
+    const db = await getConnection(FIRESTORE_CONNECTION_KEY);
+
+    const doc = await db.collection(collection).doc(id).get();
+    
+    if (!doc.exists) {
+      throw new Error(DOCUMENT_NOT_FOUND_ERROR);
+    }
+
+    return doc.data();
+  } catch (error) {
+    let status = 400;
+    if (error.message === DOCUMENT_NOT_FOUND_ERROR) status = 404;
+
+    return {
+      failed: true,
+      message: error.message,
+      data: {
+        status,
+        ...error.data,
+      },
+    };
+  }
+};
+
+const verifyInput = ({ FIRESTORE_CONNECTION_KEY, collection, id }) => {
+  const ERRORS = {
+    NO_FIRESTORE_CONNECTION_KEY: `A valid FIRESTORE_CONNECTION_KEY is required. 
+                                You can add one to your environment variables at 
+                                https://app.buildable.dev/settings/environment-variables. 
+                                You may also need to add a Firestore Datasource to your project.`,
+    NO_COLLECTION: "A valid collection name is required.",
+    NO_ID: "A valid id is required.",
+  };
+
+  if (!FIRESTORE_CONNECTION_KEY) throw new Error(ERRORS.NO_FIRESTORE_CONNECTION_KEY);
+  if (!collection || typeof collection !== "string") throw new Error(ERRORS.NO_COLLECTION);
+  if (!id) throw new Error(ERRORS.NO_ID);
+};
