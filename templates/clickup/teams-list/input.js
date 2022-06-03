c1030a93548a2fefc9752ca0feac4b751bb70575
YoussefLaunchUp <youssef@launchup.io>
2022-06-02 21:59:31 -0400

    feat: created teams_listResult template

---
+++ templates/clickup/teams-list/input.js
@@ -0,0 +1,24 @@
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * List All Teams in Workspace [Input]
+ *
+ * @author    Launch Up corp.
+ * @access    open
+ * @license   MIT
+ * @docs      https://clickup.com/api
+ *
+ * ----------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * Lets you select the input for your Node's run function
+ *
+ * @param {Params} params
+ * @param {Object} $trigger - This Flow's request object
+ * @param {Object} $nodes - Data from above Nodes
+ */
+const nodeInput = ({ $trigger, $nodes }) => {
+  return {
+    CLICKUP_API_TOKEN: $trigger.env.CLICKUP_API_TOKEN, // Required
+  };
+};
---
+++ templates/clickup/teams-list/run.js
@@ -0,0 +1,52 @@
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * List All User Groups for a Team [Run]
+ *
+ * @author    Launch Up corp.
+ * @access    open
+ * @license   MIT
+ * @docs      https://clickup.com/api
+ *
+ * ----------------------------------------------------------------------------------------------------
+ */
+
+const axios = require("axios");
+
+/**
+ * The Node’s executable function
+ *
+ * @param {Run} input - Data passed to your Node from the input function
+ */
+const run = async (input) => {
+  const { CLICKUP_API_TOKEN } = input;
+
+  verifyInput(input);
+
+  try {
+    const { data } = await axios({
+      method: "get",
+      url: "https://api.clickup.com/api/v2/team",
+      headers: { Authorization: `${CLICKUP_API_TOKEN}` },
+    });
+
+    return data;
+  } catch (error) {
+    return {
+      failed: true,
+      message: error.message,
+      data: error.response.data,
+    };
+  }
+};
+
+/**
+ * Verifies the input parameters
+ */
+const verifyInput = ({ CLICKUP_API_TOKEN }) => {
+  const ERRORS = {
+    INVALID_CLICKUP_API_TOKEN:
+      "A valid CLICKUP_API_TOKEN field (string) was not provided in the input.",
+  };
+
+  if (typeof CLICKUP_API_TOKEN !== "string") throw new Error(ERRORS.INVALID_CLICKUP_API_TOKEN);
+};
