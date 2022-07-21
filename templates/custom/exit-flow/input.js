b300f9120d1e1acd017090fcf8a7804ff179b954
Paul Krishnamurthy <paul@paulkr.com>
2022-07-21 15:30:45 -0400

    update custom templates

--- templates/custom/exit-flow/input.js
+++ templates/custom/exit-flow/input.js
@@ -1,22 +1,4 @@
-/**
- * ----------------------------------------------------------------------------------------------------
- * Exit Flow [Input]
- *
- * @author    Buildable Technologies Inc.
- * @access    open
- * @license   MIT
- *
- * ----------------------------------------------------------------------------------------------------
- */
-
-/**
- * Lets you select the input for your Node's run function
- *
- * @param {Params} params
- * @param {Object} $trigger - This Flow's request object
- * @param {Object} $nodes - Data from above Nodes
- */
-const nodeInput = ({ $trigger, $nodes }) => {
+const nodeInput = ({ $body, $headers, $env, $data }) => {
   return {
     exit: true,
     message: "An error occurred",
--- templates/custom/exit-flow/run.js
+++ templates/custom/exit-flow/run.js
@@ -1,21 +1,3 @@
-/**
- * ----------------------------------------------------------------------------------------------------
- * Exit Flow [Run]
- *
- * @description - Exit a Flow and set a custom response
- *
- * @author    Buildable Technologies Inc.
- * @access    open
- * @license   MIT
- *
- * ----------------------------------------------------------------------------------------------------
- */
-
-/**
- * The Node's executable function
- *
- * @param {Run} input - Data passed to your Node from the input function
- */
 const run = (input) => {
   const { message, data, status, exit } = input;
 
