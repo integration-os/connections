96137a3a0037fab6789e1542639af00407e45dd2
Paul Krishnamurthy <paul@paulkr.com>
2022-07-25 10:11:27 -0400

    Update sendgrid templates

--- templates/sendgrid/get-templates/input.js
+++ templates/sendgrid/get-templates/input.js
@@ -1,25 +1,6 @@
-/**
- * ----------------------------------------------------------------------------------------------------
- * Get Templates [Input]
- *
- * @author    Buildable Technologies Inc.
- * @access    open
- * @license   MIT
- * @docs      https://docs.sendgrid.com/api-reference/transactional-templates/retrieve-paged-transactional-templates
- *
- * ----------------------------------------------------------------------------------------------------
- */
-
-/**
- * Lets you select the input for your Node's run function
- *
- * @param {Params} params
- * @param {Object} $trigger - This Flow's request object
- * @param {Object} $nodes - Data from above Nodes
- */
-const nodeInput = ({ $trigger, $nodes }) => {
+const nodeInput = ({ $body, $headers, $env, $data }) => {
   return {
-    SENDGRID_API_KEY: $trigger.env.SENDGRID_API_KEY, // Required
+    BUILDABLE_SENDGRID_API_KEY: $env.BUILDABLE_SENDGRID_API_KEY, // Required
     page_size: 10, // Required
 
     // generation: "legacy,dynamic", // Generations of templates to return (i.e. "legacy", "dynamic", "legacy,dynamic")
--- templates/sendgrid/validate-email/run.js
+++ templates/sendgrid/validate-email/run.js
@@ -1,30 +1,11 @@
-/**
- * ----------------------------------------------------------------------------------------------------
- * Validate Email [Run]
- *
- * @description - Validate an email address using SendGrid's v3 Web API
- *
- * @author    Buildable Technologies Inc.
- * @access    open
- * @license   MIT
- * @docs      https://docs.sendgrid.com/api-reference/e-mail-address-validation/validate-an-email
- *
- * ----------------------------------------------------------------------------------------------------
- */
-
 const client = require("@sendgrid/client");
 
-/**
- * The Node’s executable function
- *
- * @param {Run} input - Data passed to your Node from the input function
- */
 const run = async (input) => {
-  const { SENDGRID_API_KEY, email, source } = input;
+  const { BUILDABLE_SENDGRID_API_KEY, email, source } = input;
 
   verifyInput(input);
 
-  client.setApiKey(SENDGRID_API_KEY);
+  client.setApiKey(BUILDABLE_SENDGRID_API_KEY);
 
   try {
     const [response, body] = await client.request({
@@ -51,16 +32,14 @@ const run = async (input) => {
 /**
  * Verifies the input parameters
  */
-const verifyInput = ({ SENDGRID_API_KEY, email, source }) => {
+const verifyInput = ({ BUILDABLE_SENDGRID_API_KEY, email, source }) => {
   const ERRORS = {
-    NO_SENDGRID_API_KEY: `A valid SENDGRID_API_KEY (string) is required. 
-                          You can add one to your environment variables at 
-                          https://app.buildable.dev/settings/environment-variables.`,
+    NO_BUILDABLE_SENDGRID_API_KEY: "A valid BUILDABLE_SENDGRID_API_KEY (string) is required. Create your appropriate Connection to automatically add it.",
     INVALID_EMAIL: "A valid email field (string) is required.",
     INVALID_SOURCE: "A valid source field (string) is required.",
   };
 
-  if (typeof SENDGRID_API_KEY !== "string") throw new Error(ERRORS.NO_SENDGRID_API_KEY);
+  if (typeof BUILDABLE_SENDGRID_API_KEY !== "string") throw new Error(ERRORS.NO_BUILDABLE_SENDGRID_API_KEY);
   if (typeof email !== "string") throw new Error(ERRORS.INVALID_EMAIL);
   if (typeof source !== "string") throw new Error(ERRORS.INVALID_SOURCE);
 };
