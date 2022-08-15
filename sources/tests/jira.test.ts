1de0bddc03838211fb195c933348de501608ac9c
alex maslakoff <a777@serendipia.email>
2022-08-15 10:20:56 -0300

    wip

--- sources/tests/jira.test.ts
+++ sources/tests/jira.test.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 import JiraIntegration from "../catalog/jira/Jira";
 
-const jira = new JiraIntegration({
+const jiraIntegration = new JiraIntegration({
   JIRA_PROJECT_ID: process.env.JIRA_PROJECT_ID as string,
   JIRA_HOST: process.env.JIRA_HOST as string,
   JIRA_OAUTH2_ACCESS_TOKEN: process.env.JIRA_OAUTH2_ACCESS_TOKEN as string,
@@ -13,27 +13,25 @@ describe("Jira Integration", () => {
     let webhookId: string | undefined;
 
     afterEach(async () => {
+      await jiraIntegration.deleteWebhookEndpoint({ webhookId });
+      webhookId = undefined;
     });
 
     it("should create a webhook", async () => {
-  //     const testWebhookUrl = "https://example.com/webhook";
-  //     const testEvents = ["???"];
+      const testWebhookUrl = "https://example.com/webhook";
+      const testEvents = ["jira:issue_created"];
 
-  //     // const { webhookData, events } = await jira.init({
-  //     //   webhookUrl: testWebhookUrl,
-  //     //   events: testEvents,
-  //     // });
+      const { webhookData, events } = await jiraIntegration.init({
+        webhookUrl: testWebhookUrl,
+        events: testEvents,
+      });
 
-  //     // expect(webhookData).toBeDefined();
-  //     // expect((webhookData as any).id).toBeDefined();
-  //     // expect((webhookData as any).config.url).toBe(testWebhookUrl);
+      expect(webhookData).toBeDefined();
+      expect((webhookData as any).id).toBeDefined();
+      expect((webhookData as any).config.url).toBe(testWebhookUrl);
+      expect(events).toEqual(testEvents);
 
-  //     // expect(events).toEqual(testEvents);
-
-  //     // webhookId = (webhookData as any).id;
-
-
-      true;
+      webhookId = (webhookData as any).id;
     });
   });
 })
