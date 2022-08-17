f82413a18cd47d8a44c9e95d72b56cb329580056
Paul Krishnamurthy <paul@paulkr.com>
2022-08-17 16:36:34 -0400

    Revert change in pagerduty test

--- sources/tests/pagerduty.test.ts
+++ sources/tests/pagerduty.test.ts
@@ -338,7 +338,7 @@ describe("PagerDuty Integration", () => {
 
     it("should return all webhooks", async () => {
       const result = await pagerduty.getWebhooks({});
-      expect(result.length).toBeGreaterThan(0);
+      expect((result as any).map((x) => x.id).sort()).toEqual(webhookIds.sort());
     });
 
     it("should throw an error if PagerDuty does not return 200", async () => {
