7323a6e7e4f8205e934e15eb367f1e05e93c26e5
sromicevelyn <sromic@evelyn.one>
2022-08-22 13:05:04 +0200

    CHANGE - moved type definition to types file

---
+++ sources/catalog/webflow/types.ts
@@ -0,0 +1,9 @@
+export type WebflowWebook = {
+  _id: string;
+  triggerType: string;
+  triggerId: string;
+  site: string;
+  filter?: Record<string, any>;
+  lastUsed: string;
+  createdOn: string;
+};
