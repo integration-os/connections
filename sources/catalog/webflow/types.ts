d19ee4bc0f13d8a80874217dd9567a0272c93142
sromic <simun.romic@gmail.com>
2022-08-22 23:35:09 +0200

    CHANGE - removed external types file

--- sources/catalog/webflow/types.ts
+++
@@ -1,9 +0,0 @@
-export type WebflowWebook = {
-  _id: string;
-  triggerType: string;
-  triggerId: string;
-  site: string;
-  filter?: Record<string, any>;
-  lastUsed: string;
-  createdOn: string;
-};
