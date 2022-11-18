75fe1811dab82cbe11fb85470d484bc61de09c15
Paul Krishnamurthy <paul@paulkr.com>
2022-11-17 23:27:55 -0500

    Added PostgreSQL destination connection

---
+++ catalog/destinations/postgresql/docs/actions/update.md
@@ -0,0 +1,42 @@
+### Update
+
+Update queried rows in a PostgreSQL table.
+
+[Documentation](https://knexjs.org/guide/query-builder.html#update)
+
+**Types**
+
+```ts
+interface UpdatePayload {
+  tableName: string;
+  query: object;
+  data: object;
+  returning: string[];
+  options: object ;
+}
+```
+
+**Sample Payload**
+
+```js
+{
+  tableName: "posts",
+  query: {
+    _id: 1
+  },
+  data: {
+    name: "John Doe"
+  }
+}
+```
+
+**Sample Response**
+
+```json
+{
+  "data": {
+    
+  },
+  "status": 200
+}
+```
---
+++ catalog/destinations/postgresql/index.ts
@@ -0,0 +1,37 @@
+import getProxyDriver from "./postgresql";
+
+export async function main({
+  payload,
+  config,
+  action
+}) {
+  try {
+    const driver = getProxyDriver(config);
+
+    const result = await driver[action](payload);
+
+    return result;
+  } catch (error) {
+    let e: any = {};
+
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+
+      // console.error(error.response.data);
+      // console.error(error.response.status);
+      // console.error(error.response.headers);
+      e = { data: error.response.data, status: error.response.status };
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      e = error.request;
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      e = { message: error.message };
+    }
+
+    throw e;
+  }
+}
