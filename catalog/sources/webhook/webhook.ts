0c415761fbeccc5aad99e29fd9abcaf361f1a8e7
Paul Krishnamurthy <paul@paulkr.com>
2022-11-24 13:30:30 -0500

    added webhook source connection

---
+++ catalog/sources/webhook/webhook.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { TestConnection } from "../../../types/sourceClassDefinition";
+
+export type Method =
+  | 'get' | 'GET'
+  | 'delete' | 'DELETE'
+  | 'post' | 'POST'
+  | 'put' | 'PUT'
+  | 'patch' | 'PATCH';
+
+export default class Webhook {
+  WEBHOOK_URL: string;
+  WEBHOOK_METHOD: Method;
+  WEBHOOK_HEADERS: string;
+
+  constructor({
+    WEBHOOK_URL,
+    WEBHOOK_METHOD,
+    WEBHOOK_HEADERS,
+  }: {
+    WEBHOOK_URL: string;
+    WEBHOOK_METHOD: Method;
+    WEBHOOK_HEADERS: string;
+  }) {
+    this.WEBHOOK_URL = WEBHOOK_URL;
+    this.WEBHOOK_METHOD = WEBHOOK_METHOD;
+    this.WEBHOOK_HEADERS = WEBHOOK_HEADERS;
+  }
+
+  async verifyWebhookSignature({ request, signature, secret }) {
+    // Validation falls on the user to implement
+    return true;
+  }
+
+  // async makeRequest({
+  //   signatureHeaderName,
+  //   signatureHeaderValue,
+  //   payload
+  // }: {
+  //   signatureHeaderName: string;
+  //   signatureHeaderValue: string;
+  //   payload: any;
+  // }): Promise<any> {
+  //   const response = await axios({
+  //     url: this.WEBHOOK_URL,
+  //     method: this.WEBHOOK_METHOD,
+  //     headers: {
+  //       ...JSON.parse(this.WEBHOOK_HEADERS),
+  //       [signatureHeaderName]: signatureHeaderValue,
+  //     },
+  //     data: payload
+  //   });
+
+  //   return response;
+  // }
+  
+  async testConnection(): Promise<TestConnection> {
+    try {
+      await axios({
+        method: this.WEBHOOK_METHOD,
+        url: this.WEBHOOK_URL,
+        headers: JSON.parse(this.WEBHOOK_HEADERS)
+      });
+  
+      return {
+        success: true,
+        message: "Connection tested successfully!",
+      };
+    } catch (err) {
+      throw new Error(`Unable to make a ${this.WEBHOOK_METHOD} request: ${err.message}`);
+    }
+  }
+}
