4f5f024ef673d1ec2163f2d5bb6d7da5a7603861
Mike Gindin <gindin.mike@gmail.com>
2023-01-10 16:40:05 -0500

    update elasticserach config and change ELASTIC_SEARCH to ELASTICSEARCH

--- catalog/destinations/elasticsearch/test/index.spec.ts
+++ catalog/destinations/elasticsearch/test/index.spec.ts
@@ -24,13 +24,13 @@ describe('Test: ElasticSearch', () => {
   });
 
   it("should connect to an elasticsearch node", async () => {
-      const ELASTIC_SEARCH_TLS_CA = process.env.ELASTIC_SEARCH_TLS_CA_PATH ? await fs.promises.readFile(process.env.ELASTIC_SEARCH_TLS_CA_PATH) : process.env.ELASTIC_SEARCH_TLS_CA || ""
+      const ELASTICSEARCH_TLS_CA = process.env.ELASTICSEARCH_TLS_CA_PATH ? await fs.promises.readFile(process.env.ELASTICSEARCH_TLS_CA_PATH) : process.env.ELASTICSEARCH_TLS_CA || ""
 
       const driver = getProxyDriver({
-        ELASTIC_SEARCH_URI: process.env.ELASTIC_SEARCH_URI || "https://localhost:9200",
-        ELASTIC_SEARCH_BASIC_USER: process.env.ELASTIC_SEARCH_BASIC_USER || "elastic",
-        ELASTIC_SEARCH_BASIC_PASSWORD: process.env.ELASTIC_SEARCH_BASIC_PASSWORD || "changeme",
-        ELASTIC_SEARCH_TLS_CA,
+        ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL || "https://localhost:9200",
+        ELASTICSEARCH_BASIC_USER: process.env.ELASTICSEARCH_BASIC_USER || "elastic",
+        ELASTICSEARCH_BASIC_PASSWORD: process.env.ELASTICSEARCH_BASIC_PASSWORD || "changeme",
+        ELASTICSEARCH_TLS_CA,
       });
 
       const result = await driver.testConnection();
@@ -39,22 +39,22 @@ describe('Test: ElasticSearch', () => {
   });
 
   it("should index, update, and delete", async () => {
-    const ELASTIC_SEARCH_TLS_CA = process.env.ELASTIC_SEARCH_TLS_CA_PATH ? await fs.promises.readFile(process.env.ELASTIC_SEARCH_TLS_CA_PATH) : process.env.ELASTIC_SEARCH_TLS_CA || ""
+    const ELASTICSEARCH_TLS_CA = process.env.ELASTICSEARCH_TLS_CA_PATH ? await fs.promises.readFile(process.env.ELASTICSEARCH_TLS_CA_PATH) : process.env.ELASTICSEARCH_TLS_CA || ""
 
     const driver = getProxyDriver({
-      ELASTIC_SEARCH_URI: process.env.ELASTIC_SEARCH_URI || "https://localhost:9200",
-      ELASTIC_SEARCH_BASIC_USER: process.env.ELASTIC_SEARCH_BASIC_USER || "elastic",
-      ELASTIC_SEARCH_BASIC_PASSWORD: process.env.ELASTIC_SEARCH_BASIC_PASSWORD || "changeme",
-      ELASTIC_SEARCH_TLS_CA,
+      ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL || "https://localhost:9200",
+      ELASTICSEARCH_BASIC_USER: process.env.ELASTICSEARCH_BASIC_USER || "elastic",
+      ELASTICSEARCH_BASIC_PASSWORD: process.env.ELASTICSEARCH_BASIC_PASSWORD || "changeme",
+      ELASTICSEARCH_TLS_CA,
     });
 
     const client = new Client({
-      node: process.env.ELASTIC_SEARCH_URI || "https://localhost:9200",
+      node: process.env.ELASTICSEARCH_URL || "https://localhost:9200",
       auth: {
-        username: process.env.ELASTIC_SEARCH_BASIC_USER || "elastic",
-        password: process.env.ELASTIC_SEARCH_BASIC_PASSWORD || "changeme",
+        username: process.env.ELASTICSEARCH_BASIC_USER || "elastic",
+        password: process.env.ELASTICSEARCH_BASIC_PASSWORD || "changeme",
       },
-      ...(ELASTIC_SEARCH_TLS_CA ? { tls: { ca: ELASTIC_SEARCH_TLS_CA } } : {})
+      ...(ELASTICSEARCH_TLS_CA ? { tls: { ca: ELASTICSEARCH_TLS_CA } } : {})
     });
 
     let index = "test-" + makeid(5)
@@ -136,22 +136,22 @@ describe('Test: ElasticSearch', () => {
   });
 
   it("should bulk", async () => {
-    const ELASTIC_SEARCH_TLS_CA = process.env.ELASTIC_SEARCH_TLS_CA_PATH ? await fs.promises.readFile(process.env.ELASTIC_SEARCH_TLS_CA_PATH) : process.env.ELASTIC_SEARCH_TLS_CA || ""
+    const ELASTICSEARCH_TLS_CA = process.env.ELASTICSEARCH_TLS_CA_PATH ? await fs.promises.readFile(process.env.ELASTICSEARCH_TLS_CA_PATH) : process.env.ELASTICSEARCH_TLS_CA || ""
 
     const driver = getProxyDriver({
-      ELASTIC_SEARCH_URI: process.env.ELASTIC_SEARCH_URI || "https://localhost:9200",
-      ELASTIC_SEARCH_BASIC_USER: process.env.ELASTIC_SEARCH_BASIC_USER || "elastic",
-      ELASTIC_SEARCH_BASIC_PASSWORD: process.env.ELASTIC_SEARCH_BASIC_PASSWORD || "changeme",
-      ELASTIC_SEARCH_TLS_CA,
+      ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL || "https://localhost:9200",
+      ELASTICSEARCH_BASIC_USER: process.env.ELASTICSEARCH_BASIC_USER || "elastic",
+      ELASTICSEARCH_BASIC_PASSWORD: process.env.ELASTICSEARCH_BASIC_PASSWORD || "changeme",
+      ELASTICSEARCH_TLS_CA,
     });
 
     const client = new Client({
-      node: process.env.ELASTIC_SEARCH_URI || "https://localhost:9200",
+      node: process.env.ELASTICSEARCH_URL || "https://localhost:9200",
       auth: {
-        username: process.env.ELASTIC_SEARCH_BASIC_USER || "elastic",
-        password: process.env.ELASTIC_SEARCH_BASIC_PASSWORD || "changeme",
+        username: process.env.ELASTICSEARCH_BASIC_USER || "elastic",
+        password: process.env.ELASTICSEARCH_BASIC_PASSWORD || "changeme",
       },
-      ...(ELASTIC_SEARCH_TLS_CA ? { tls: { ca: ELASTIC_SEARCH_TLS_CA } } : {})
+      ...(ELASTICSEARCH_TLS_CA ? { tls: { ca: ELASTICSEARCH_TLS_CA } } : {})
     });
 
     let index = "tweets-" + makeid(5)
