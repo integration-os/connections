fffcf88c0547710ae1af06d9fbee70cdf3dca69d
Houssem Eddine Zerrad <hzerrad96@gmail.com>
2023-03-11 17:10:57 +0100

    setup config.json

--- catalog/sources/rutter/rutter.ts
+++ catalog/sources/rutter/rutter.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import {
   AnyObject, DeleteWebhookEndpointProps,
   Events,
@@ -17,17 +18,22 @@ export default class RutterIntegration implements IntegrationClassI {
 
   private readonly RUTTER_PASSWORD: string;
 
+  private readonly RUTTER_CLIENT_SECRET: string;
+
   private RUTTER_CONNECTION: RutterConnection;
 
   constructor({
     RUTTER_EMAIL,
     RUTTER_PASSWORD,
+    RUTTER_CLIENT_SECRET,
   }: {
     RUTTER_EMAIL: string;
     RUTTER_PASSWORD: string;
+    RUTTER_CLIENT_SECRET: string;
   }) {
     this.RUTTER_EMAIL = RUTTER_EMAIL;
     this.RUTTER_PASSWORD = RUTTER_PASSWORD;
+    this.RUTTER_CLIENT_SECRET = RUTTER_CLIENT_SECRET;
 
     this.RUTTER_CONNECTION = new RutterConnection(RUTTER_EMAIL, RUTTER_PASSWORD);
   }
@@ -50,8 +56,18 @@ export default class RutterIntegration implements IntegrationClassI {
     };
   }
 
-  async verifyWebhookSignature(props: VerifyWebhookSignatureProps): Promise<Truthy> {
-    return Promise.resolve(undefined);
+  async verifyWebhookSignature({ request, signature }: VerifyWebhookSignatureProps): Promise<Truthy> {
+    const secret = this.RUTTER_CLIENT_SECRET;
+    const hash = crypto
+      .createHmac("sha256", secret)
+      .update(request.body, "utf8")
+      .digest("base64");
+
+    if (`sha256=${hash}` !== signature) {
+      throw new Error("Invalid signature");
+    }
+
+    return true;
   }
 
   async subscribe({ webhookId, events }: SubscriptionProps): Promise<SubscribeReturns> {
