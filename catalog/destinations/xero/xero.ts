027b9326e737965a231dcafea15e7480c54ddaa3
Houssem Eddine Zerrad <hzerrad96@gmail.com>
2023-05-13 20:04:50 +0100

    Implement destination class mandatory methods on XeroDriver

--- catalog/destinations/xero/xero.ts
+++ catalog/destinations/xero/xero.ts
@@ -1,20 +1,63 @@
+import { TokenSet, XeroClient } from "xero-node";
+import axios from "axios";
 import { AnyObject, DestinationClassI, TestConnection, Truthy } from "../../../types/destinationClassDefinition";
+import { XeroOAuth2TokenSet } from "./lib/types";
 
 export class XeroDriver implements DestinationClassI {
-  constructor(_: AnyObject) {
-    throw Error("Xero driver is not implemented yet");
+  private client: XeroClient;
+
+  private tenantIds: string[];
+
+  private readonly XERO_CLIENT_ID: any;
+
+  private readonly XERO_CLIENT_SECRET: any;
+
+  constructor({ XERO_CLIENT_ID, XERO_CLIENT_SECRET }: AnyObject) {
+    this.XERO_CLIENT_ID = XERO_CLIENT_ID;
+    this.XERO_CLIENT_SECRET = XERO_CLIENT_SECRET;
   }
 
-  async connect(_config?: AnyObject): Promise<void | Truthy> {
-    throw Error("Xero driver is not implemented yet");
+  async connect(config?: AnyObject): Promise<void | Truthy> {
+    this.client = new XeroClient({
+      clientId: this.XERO_CLIENT_ID,
+      clientSecret: this.XERO_CLIENT_SECRET,
+      redirectUris: [config.oauth2.redirectUri],
+      scopes: config.oauth2.scopes,
+      httpTimeout: 3000,
+    });
+
+    const tokenSet: XeroOAuth2TokenSet = config.oauth2.resolved;
+
+    this.client.setTokenSet(tokenSet as TokenSet);
+
+    // get tenants
+    const response = await axios.get("https://api.xero.com/connections", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${tokenSet.access_token}`,
+      },
+    });
+    this.tenantIds = response.data.map((item: any) => item.tenantId);
   }
 
   async disconnect(): Promise<void | Truthy> {
-    throw Error("Xero driver is not implemented yet");
+    this.client = null;
   }
 
   async testConnection(): Promise<TestConnection> {
-    throw Error("Xero driver is not implemented yet");
+    try {
+      await this.client.accountingApi.getAccounts(this.tenantIds[0]);
+
+      return {
+        success: true,
+        message: "Connection established successfully",
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: "Could not establish connection to Xero",
+      };
+    }
   }
 }
 
