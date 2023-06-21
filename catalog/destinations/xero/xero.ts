9c3901c4a838ae2d64dce17bb2aec5ffcbb77fd3
Krish <krishparekh9@gmail.com>
2023-06-21 19:18:45 +0530

    Remove logs

--- catalog/destinations/xero/xero.ts
+++ catalog/destinations/xero/xero.ts
@@ -36,10 +36,6 @@ export class XeroDriver implements DestinationClassI {
       config?.XERO_REFRESH_TOKEN || this.XERO_REFRESH_TOKEN,
     );
 
-    console.log("Xero token set: ", validTokenSet);
-    console.log(validTokenSet.access_token);
-    console.log(validTokenSet.refresh_token);
-
     this.client.setTokenSet(validTokenSet);
 
     // get and save all registered tenants
